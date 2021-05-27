64200e3add4b2d70eb9ffa0cd41fadba312c1764
nicod-man <nico.mantovani95@gmail.com>
2021-05-27 14:19:58 +0200

    dataset/test

--- stage_fer.py
+++ stage_fer.py
@@ -29,8 +29,8 @@ classnames = ["Angry", "Disgust", "Fear", "Happy",
               "Neutral", "Sad", "Surprise"]
 
 default_server_port = 9250
-height = 1280
-width = 959
+#height = 1280
+#width = 959
 
 
 
@@ -69,7 +69,7 @@ Load an image and return input data for the network
 # , target_size=(height, width)
 def inputImage(imagefile):
     try:
-        img = load_img(imagefile, color_mode="rgb", target_size=(height,width))
+        img = load_img(imagefile, color_mode="rgb")
         arr = img_to_array(img) / 255
         inp = np.array([arr])  # Convert single image to a batch.
         return inp
@@ -141,13 +141,12 @@ class ModelServer(threading.Thread):
             count -= len(newbuf)
         return buf
 
-
     def run(self):
 
         imgsize = -1
         res = 'none 0.0'
         while (self.dorun):
-            self.connect()  
+            self.connect()  # wait for connection
             try:
                 # Receive data
                 while (self.dorun):
@@ -161,34 +160,53 @@ class ModelServer(threading.Thread):
                         data = None
 
                     buf = b''
-                    if (type(data)!=str):
+                    if (type(data) != str):
                         k = data.find(b'\n')
-                        if (k<0):
+                        if (k < 0):
                             data = data.decode('utf-8')
-                        elif (len(data)>k+1):
-                            buf = data[k+2:]
+                        elif (len(data) > k + 1):
+                            buf = data[k + 2:]
                             data = data[0:k].decode('utf-8')
 
-                            # print('Eval image [%s]' %v[1])
-                            # (p,c) = predictImage(self.model,v[1])
-                            # print("Predicted: %s, prob: %.3f" %(c,p))
-                            # res = "%s %.3f" %(c,p)
-                            # ressend = (res+'\n\r').encode('UTF-8')
-                            # self.connection.send(ressend)
-
-                    if (data!=None and data!="" and data!="***"):
+                    if (data != None and data != "" and data != "***"):
                         self.received = data
-                        print('Received: %s' %data)
+                        print('Received: %s' % data)
                         v = data.split(' ')
-                        if v[0]=='EVAL' and len(v)>1:
-                            print('Evaluating frame [%s]' %v[1])
-                            (p,c) = predictImage(self.model,v[1])
-                            print("Predicted: %s, probability: %.3f" %(c,p))
-                            res = "%s %.3f" %(c,p)
-                            ressend = (res+'\n\r').encode('UTF-8')
+                        if v[0] == 'REQ':
+                            self.connection.send('ACK\n\r'.encode('UTF-8'))
+                        elif v[0] == 'GETRESULT':
+                            ressend = (res + '\n\r').encode('UTF-8')
                             self.connection.send(ressend)
-                        elif (data == None or data==""):
-                            break
+                        elif v[0] == 'EVAL' and len(v) > 1:
+                            print('Eval image [%s]' % v[1])
+                            (p, c) = predictImage(self.model, v[1])
+                            print("Predicted: %s, prob: %.3f" % (c, p))
+                            res = "%s %.3f" % (c, p)
+                            ressend = (res + '\n\r').encode('UTF-8')
+                            self.connection.send(ressend)
+                        elif v[0] == 'RGB' and len(v) >= 3:
+                            imgwidth = int(v[1])
+                            imgheight = int(v[2])
+                            imgsize = imgwidth * imgheight * 3
+                            print("RGB image size: %d" % imgsize)
+                            buf = self.recvall(imgsize, buf)
+                            if buf is not None:
+                                print("Image received size: %d " % (len(buf)))
+                                a = np.fromstring(buf, dtype='uint8')
+                                a = a.reshape((imgheight, imgwidth, 3))
+                                a = a / 255.0
+                                inp = np.array([a])
+                                pr = model.predict(inp)
+                                (p, c) = (np.max(pr), classnames[np.argmax(pr)])
+                                print("Predicted: %s, prob: %.3f" % (c, p))
+                                res = "%s %.3f" % (c, p)
+                                ressend = (res + '\n\r').encode('UTF-8')
+                                self.connection.send(ressend)
+                        else:
+                            print('Received: %s' % data)
+
+                    elif (data == None or data == ""):
+                        break
             finally:
                 print('Connection closed.')
                 # Clean up the connection
